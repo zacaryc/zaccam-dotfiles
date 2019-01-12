b86de8e795799a60e3a2d95a43ddd4f1e415d463
Zac Campbell <zacaryc24@gmail.com>
2019-01-12 18:38:06 +1100

    iUpdated ranger configurations from local setups - these include devicon support and preview images changes

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -57,20 +57,6 @@ handle_extension() {
             7z l -p -- "${FILE_PATH}" && exit 5
             exit 1;;
 
-        # PDF
-        #pdf)
-        #     pdftoppm -f 1 -l 1 \
-        #              -scale-to-x 1920 \
-        #              -scale-to-y -1 \
-        #              -singlefile \
-        #              -jpeg -tiffcompression jpeg \
-        #              -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
-        #         && exit 6 || exit 1;;
-            # Preview as text conversion
-            #pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - && exit 5
-            #exiftool "${FILE_PATH}" && exit 5
-            #exit 1;;
-
         # BitTorrent
         torrent)
             transmission-show -- "${FILE_PATH}" && exit 5
@@ -116,10 +102,10 @@ handle_image() {
             exit 7;;
 
         # Video
-        # video/*)
+         video/*)
         #     # Thumbnail
-        #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-        #     exit 1;;
+             ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+             exit 1;;
         # PDF
          application/pdf)
              pdftoppm -f 1 -l 1 \
