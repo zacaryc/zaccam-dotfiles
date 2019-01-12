6e5fcdcaf07340fdbb82c53d1d65a6d36f350283
Zac Campbell <zacaryc24@gmail.com>
2019-01-12 18:40:48 +1100

    Added functionality and settings to bootstrap macos settings such as locales languages and dock display.

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,16 +19,21 @@ declare -a dotfiles=(
     'profile'
 )
 
+# Install: Quick Bootstrap install of dotfiles
 function install() {
 
     case $(uname) in
         'Darwin')
+            # TODO: Add in support for macos settings changes
             _print_header "This is a mac - Installing with brewfiles"
             ln -sf ~/zaccam-dotfiles/Brewfile ~/Brewfile
+            # TODO: Install brew if it doesn't exist
             brew bundle
+            [ -f ~/zaccam-dotfiles/.macos ] || sh ~/zaccam-dotfiles/.macos
             ;;
         'Linux')
             _print_header "This is a linux machine"
+            # TODO: Linux Package Support
             echo "No support for package install yet - TODO"
             ;;
     esac
@@ -42,11 +47,14 @@ function install() {
     _print_header "Dotfiles have been installed!"
 }
 
+
+## UPDATE- Update the existing dotfiles via git pull
 function update() {
     cd ~/zaccam-dotfiles/ && git pull origin master
     _print_header "Dotfiles have been updated!"
 }
 
+# BACKUP - All specified dotfiles to be backed up
 function backup() {
     mkdir -p ~/dotfiles.backup/
 
@@ -61,6 +69,43 @@ function backup() {
 function setup() {
 
     [ -d ~/git/ ] && mkdir -p ~/git/
+
+}
+
+
+# Install Python with pyenv
+function install_python() {
+
+    if which pyenv > /dev/null; then
+        CFLAGS="-I$(brew --prefix openssl)/include" && export CFLAGS
+        LDFLAGS="-L$(brew --prefix openssl)/lib" && export LDFLAGS
+        PYENV_ROOT="/usr/local/python" && export PYENV_ROOT
+
+        sudo mkdir -p "$PYENV_ROOT"
+        sudo chown -R "$(whoami):admin" "$PYENV_ROOT"
+
+        p "Installing Python 2 with pyenv"
+        pyenv install --skip-existing 2.7.13
+        p "Installing Python 3 with pyenv"
+        pyenv install --skip-existing 3.6.2
+        pyenv global 2.7.13
+
+        grep -q "${PYENV_ROOT}" "/etc/paths" || \
+        sudo sed -i "" -e "1i\\
+    ${PYENV_ROOT}/shims
+    " "/etc/paths"
+
+        init_paths
+        rehash
+
+        pip install --upgrade "pip" "setuptools"
+
+        # Basic Packages
+        pip install --upgrade "requests"
+        pip install --upgrade "requests-cache" "requests[security]"
+
+    fi
+
 
 }
 
