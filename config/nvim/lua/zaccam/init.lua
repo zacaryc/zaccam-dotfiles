6beffeb086b24ec9235e855c217b4cbb492351aa
Zac Campbell <zcampbell@pros.com>
2024-05-30 23:21:21 +1000

    Replaced largely by lualine, keeping for posterity and reference

--- config/nvim/lua/zaccam/init.lua
+++ config/nvim/lua/zaccam/init.lua
@@ -5,7 +5,7 @@ vim.g.maplocalleader = '\\'
 require("zaccam.packer")
 require("zaccam.set")
 require("zaccam.netrw")
-require("zaccam.statusline")
+-- require("zaccam.statusline")
 require("zaccam.display")
 require("zaccam.remap")
 require("zaccam.helpers")
--- config/nvim/lua/zaccam/statusline.lua
+++ config/nvim/lua/zaccam/statusline.lua
@@ -3,3 +3,23 @@
 -- vim.cmd('set statusline+=%#warningmsg#')
 -- vim.cmd('set statusline+=%{fugitive#statusline()}')
 -- vim.cmd('set statusline+=%*')
+
+-- Set Airline Theme
+-- vim.cmd[[
+--     let g:airline_theme="hybrid"
+--     let g:airline_powerline_fonts = 1
+--     let g:webdevicons_enable_airline_statusline = 1
+-- ]]
+
+-- -- Set this. Airline will handle the rest.
+-- vim.cmd("let g:airline#extensions#ale#enabled = 1")
+
+-- vim.cmd[[
+--     if !exists('g:airline_symbols')
+--         let g:airline_symbols = {}
+--     endif
+--     let g:airline_symbols.space = " "
+-- ]]
+
+-- require('lualine').get_config()
+-- require('lualine').setup()
