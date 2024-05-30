90d50a2d1db2cf8078e1ebc159c7e1e1da5604ea
Zac Campbell <zcampbell@pros.com>
2024-05-30 23:22:45 +1000

    Removed statusline info as it's now in statusline, and also hacking a cheat by initialising a fully neovim supported colorscheme first to add some of the mappings, and then swapping to the actual colours I like. It's not perfect but it beats the current bad implementations and matching of the new 'hybrid' neovim versions which are completely different colours

--- config/nvim/lua/zaccam/display.lua
+++ config/nvim/lua/zaccam/display.lua
@@ -2,6 +2,7 @@
 
 -- Set Font
 -- vim.opt.guifont = 'DejaVuSansMono Nerd Font'
+vim.opt.termguicolors = true
 vim.opt.guifont = 'Hack Nerd Font:h13'
 
 -- Set Colouring Themes
@@ -10,25 +11,8 @@ vim.cmd("let g:hybrid_use_Xresources = 1")
 
 -- Set Colorscheme
 vim.opt.background = 'dark'
+vim.cmd.colorscheme("catppuccin-mocha")
 vim.cmd.colorscheme("hybrid")
--- vim.cmd.colorscheme("kanagawa-dragon")
-
--- Set Airline Theme
-vim.cmd[[
-    let g:airline_theme="hybrid"
-    let g:airline_powerline_fonts = 1
-    let g:webdevicons_enable_airline_statusline = 1
-]]
-
--- Set this. Airline will handle the rest.
-vim.cmd("let g:airline#extensions#ale#enabled = 1")
-
-vim.cmd[[
-    if !exists('g:airline_symbols')
-        let g:airline_symbols = {}
-    endif
-    let g:airline_symbols.space = " "
-]]
 
 -- Set List Characters and Fill Characters
 vim.opt.listchars:append {
@@ -43,4 +27,3 @@ vim.opt.fillchars:append {
     vert = '▚',
     fold = '·'
 }
-
