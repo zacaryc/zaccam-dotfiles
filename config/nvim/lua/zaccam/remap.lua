480e5a9915418827acea7f6daf8f364075e2d13b
Zac Campbell <zcampbell@pros.com>
2024-06-03 14:27:04 +1000

    Nice little helpers to move around and to retrain not using the evil arrow keys in normal mode. Like my old hardkient plugin I've lost over time

--- config/nvim/lua/zaccam/remap.lua
+++ config/nvim/lua/zaccam/remap.lua
@@ -52,6 +52,20 @@ map('n', 'N', 'Nzzzv')
 
 --Yank entire document to clipboard
 map('n', '<leader>y', 'mzgg"+yG`z')
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 ------------------------------ }}}
 -- > Command Mode {{{
