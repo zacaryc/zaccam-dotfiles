3a46e5c34b1b19406db5c8cd3f9f9da75747fd8f
Zac Campbell <zcampbell@pros.com>
2024-06-03 14:27:36 +1000

    The kickstart.nvim lsp decided to change all these, so I'll trust it knew better

--- config/nvim/lua/zaccam/remap.lua
+++ config/nvim/lua/zaccam/remap.lua
@@ -5,53 +5,54 @@
 -----------------------------------------------------------
 
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap=true, silent=true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- > General Maps {{{
 --------------------------------
 -- remap VIM 0 to first non-blank character
-map('', '0', '^')
+map("", "0", "^")
 
 -- Disable that goddamn 'Entering Ex mode. Type 'visual' to go to Normal mode.'
 --  that I trigger 40x a day.
-map('', 'Q', '<Nop>')
-map('', 'q', '<Nop>')
+map("", "Q", "<Nop>")
+map("", "q", "<Nop>")
 
 -- Move to matching tags i.e. <...> via tab
-map('', '<tab>', '%')
+map("", "<tab>", "%")
 
 -- Delete without affecting registers
-map('n', '<leader>d', '"_d')
-map('v', '<leader>d', '"_d')
+map("n", "<leader>d", '"_d')
+map("v", "<leader>d", '"_d')
 
 ------------------------------ }}}
 -- > Normal Mode Map {{{
 --------------------------------
 
 -- Have capital Y yank til EOL
-map('n', 'Y', 'y$')
+map("n", "Y", "y$")
 -- Open Netrw Draw
-map('n', '<Leader>l', ':call ToggleNetrw()<CR>')
+map("n", "<Leader>l", ":call ToggleNetrw()<CR>")
 -- Open Tagbar
-map('n', '<Leader>t', ':TagbarToggle<CR>')
+map("n", "<Leader>t", ":TagbarToggle<CR>")
 -- Open To Do List
-map('n', '<localleader>t', ':Todo<CR>')
+map("n", "<localleader>t", ":Todo<CR>")
 -- Git Status
-map('n', '<leader>gs', ':G<CR>')
+map("n", "<leader>gs", ":G<CR>")
 -- Git Blame
-map('n', '<Leader>b', ':Git blame -w<CR>')
+map("n", "<Leader>b", ":Git blame -w<CR>")
 
 -- Keep searches centred
-map('n', 'n', 'nzzzv')
-map('n', 'N', 'Nzzzv')
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 --Yank entire document to clipboard
-map('n', '<leader>y', 'mzgg"+yG`z')
+map("n", "<leader>y", 'mzgg"+yG`z')
+
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
@@ -72,7 +73,7 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 --------------------------------
 
 -- Allow saving of files as sudo when I forgot to start vim using sudo.
-vim.cmd('cmap w!! w !sudo tee > /dev/null %')
+vim.cmd("cmap w!! w !sudo tee > /dev/null %")
 
 --------------------------------
 -- }}}
@@ -80,36 +81,36 @@ vim.cmd('cmap w!! w !sudo tee > /dev/null %')
 --------------------------------
 
 -- Instead of reaching for the escape key
-map('i', 'kj', '<Esc>')
+map("i", "kj", "<Esc>")
 
 -- Add break points for undo
-map('i', ',', ',<C-g>u')
-map('i', '.', '.<C-g>u')
-map('i', '!', '!<C-g>u')
-map('i', '?', '?<C-g>u')
+map("i", ",", ",<C-g>u")
+map("i", ".", ".<C-g>u")
+map("i", "!", "!<C-g>u")
+map("i", "?", "?<C-g>u")
 
 --------------------------------
 -- }}}
 -- > Visual Mode {{{
 --------------------------------
-map('v', '<Space>', 'I<Space><Esc>gv')
+map("v", "<Space>", "I<Space><Esc>gv")
 --
 -- Move highlighted lines up and down with auto indent formatting
-map('v', 'J', ":m '>+1<CR>gv=gv")
-map('v', 'K', ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 -- Shift selected text one space and re-select
-map('v', '<Space>', 'I<Space><Esc>gv')
+map("v", "<Space>", "I<Space><Esc>gv")
 
 --------------------------------
 -- }}}
 -- > Executable Mode {{{
 --------------------------------
 
-map('x', '<leader>p', '"_dP')
+map("x", "<leader>p", '"_dP')
 
 --------------------------------
 -- }}}
 
-if vim.fn.has('digraphs') then
-    vim.cmd('digraph ./ 8230')
+if vim.fn.has("digraphs") then
+	vim.cmd("digraph ./ 8230")
 end
